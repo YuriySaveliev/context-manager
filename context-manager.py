245b62354037379b61142c54248f15b9fdd04301
YuriySaveliev <savjurnick@gmail.com>
2021-09-07 23:00:35 +0300

    Add methods

--- context-manager.py
+++ context-manager.py
@@ -3,14 +3,21 @@ import sqlite3
 class ContextManager:
     def create_connection(self):
         self.connection = sqlite3.connect('test.db')
-        cursor = connection.cursor()
+        self.cursor = self.connection.cursor()
+
+        return self.cursor
 
     def exit(self):
         try:
-            pass
+            self.cursor.execute('''CREATE TABLE products(name text, quantity integer, price real)''')
+            self.cursor.execute("INSERT INTO products VALUES ('Tomato', 50, 10.57)")
+            self.connection.commit()
         except:
-            pass
+            print('Error!')
+            self.connection.rollback()
 
         self.connection.close()
 
 cm = ContextManager()
+cursor = cm.create_connection()
+cm.exit()
