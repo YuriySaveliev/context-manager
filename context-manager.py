0f60a4471f87fc1b7db0ae6cd341fba2ec019470
YuriySaveliev <savjurnick@gmail.com>
2021-09-21 23:23:23 +0300

    Apply another approach for CM

--- context-manager.py
+++ context-manager.py
@@ -1,23 +1,27 @@
 import sqlite3
 
 class ContextManager:
-    def create_connection(self):
-        self.connection = sqlite3.connect('test.db')
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
 
         return self.cursor
 
-    def exit(self):
-        try:
-            self.cursor.execute('''CREATE TABLE products(name text, quantity integer, price real)''')
-            self.cursor.execute("INSERT INTO products VALUES ('Tomato', 50, 10.57)")
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not exc_type:
             self.connection.commit()
-        except:
-            print('Error!')
+        else:
+            print(exc_val)
             self.connection.rollback()
 
         self.connection.close()
 
-cm = ContextManager()
-cursor = cm.create_connection()
-cm.exit()
+        return True
+
+cm = ContextManager('test.db')
+with cm:
+    cm.cursor.execute("CREATE TABLE products(name text, quantity integer, price real)")
+    cm.cursor.execute("INSERT INTO products VALUES ('Potato', 10, 11.50)")
